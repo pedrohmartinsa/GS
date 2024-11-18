24ececf23b7346d4e732f184c6e5c77fe2fcfd27
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-11-18 19:29:44 -0300

    abas do produto feitas

--- gs/src/pages/ProductAboutPage.jsx
+++ gs/src/pages/ProductAboutPage.jsx
@@ -8,19 +8,19 @@ export default function ProductAboutPage() {
 
     return(
         <>
-            <main className="md:flex md:flex-col md:items-center h-screen">
-                <div className="relative flex justify-center gap-14 mb-14 pt-16">
+            <main className="md:flex md:flex-col md:items-center h-auto mb-10 pt-4">
+                <div className="relative flex justify-center items-center md:w-full gap-14 mb-14 pt-16">
                     <Link
-                    className="absolute left-0 top-5 md:w-full md:bottom-0 hover:text-slate-500" 
+                    className="absolute top-0 left-0 md:relative hover:text-slate-500" 
                     to="/product"
                     >
-                        <IoChevronBackCircle className="md:size-16 size-12"/>
+                        <IoChevronBackCircle className="md:size-20 size-12"/>
                     </Link>
                     <h2 className="text-5xl text-center md:text-8xl pt-1">{title}</h2>
                 </div>
                 <div className="flex flex-col md:flex-row gap-5 items-center justify-center">
-                    <img className="w-56 md:w-auto" src={`/${foto}`} alt=""/>
-                    <p className="border-2 border-secondary p-6 rounded-lg w-56 md:w-96 h-auto text-xl md:text-4xl text-justify">{sobre}</p>
+                    <img className="w-56  md:w-96" src={`/${foto}`} alt=""/>
+                    <p className="border-2 border-secondary p-6 rounded-lg w-72 md:w-96 h-auto text-xl md:text-4xl ">{sobre}</p>
                 </div>
                 
             </main>
