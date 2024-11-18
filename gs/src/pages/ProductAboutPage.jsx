ebec6da2ecac1f666e9ce557282df168965e7c8e
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-11-18 18:32:18 -0300

    arrumando o display da abas do produto para desktop

--- gs/src/pages/ProductAboutPage.jsx
+++ gs/src/pages/ProductAboutPage.jsx
@@ -8,21 +8,24 @@ export default function ProductAboutPage() {
 
     return(
         <>
-            <main className="h-screen">
-                <Link
-                    className="hover:text-slate-500" 
+            <main className="md:flex md:flex-col md:items-center h-screen">
+                <div className="relative flex justify-center gap-14 mb-14 pt-16">
+                    <Link
+                    className="absolute left-0 top-5 md:w-full md:bottom-0 hover:text-slate-500" 
                     to="/product"
                     >
-                        <IoChevronBackCircle size={50}/>
-                </Link>
-                <div className="flex flex-col items-center gap-14 mb-14">
-                    <h2 className="text-5xl md:text-8xl">{title}</h2>
-                    <img className="w-56 md:w-auto" src={`/${foto}`} alt="" />
+                        <IoChevronBackCircle className="md:size-16 size-12"/>
+                    </Link>
+                    <h2 className="text-5xl text-center md:text-8xl pt-1">{title}</h2>
                 </div>
+                <div className="flex flex-col md:flex-row gap-5 items-center justify-center">
+                    <img className="w-56 md:w-auto" src={`/${foto}`} alt=""/>
+                    <p className="border-2 border-secondary p-6 rounded-lg w-56 md:w-96 h-auto text-xl md:text-4xl text-justify">{sobre}</p>
+                </div>
+                
             </main>
 
-            <p>{sobre}</p>
-            <h1>{id}</h1>
+            
         </>
     )
 }
