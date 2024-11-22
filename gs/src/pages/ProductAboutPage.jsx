8796e5bf7ba63111c32dc26360c21112ddda5ad9
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-11-22 11:41:48 -0300

    Add padding in some places

--- gs/src/pages/ProductAboutPage.jsx
+++ gs/src/pages/ProductAboutPage.jsx
@@ -11,21 +11,21 @@ export default function ProductAboutPage() {
             <main className="md:flex md:flex-col md:items-center h-auto mb-10 pt-4">
                 <div className="relative flex justify-center items-center md:w-full gap-14 mb-14 pt-16">
                     <Link
-                    className="absolute top-0 left-0 md:relative hover:text-slate-500" 
+                    className="absolute top-0 left-0 md:relative hover:text-slate-500"
                     to="/product"
                     >
                         <IoChevronBackCircle className="md:size-20 size-12"/>
                     </Link>
                     <h2 className="text-5xl text-center md:text-8xl pt-1">{title}</h2>
                 </div>
-                <div className="flex flex-col md:flex-row gap-5 items-center justify-center">
+                <div className="flex flex-col md:flex-row gap-5 items-center justify-center px-5">
                     <img className="w-56  md:w-96" src={`/${foto}`} alt=""/>
                     <p className="border-2 border-secondary p-6 rounded-lg w-72 md:w-auto max-w-[650px] h-auto text-xl md:text-4xl ">{sobre}</p>
                 </div>
-                
+
             </main>
 
-            
+
         </>
     )
 }
